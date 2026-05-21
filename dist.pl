96124369360faa12b27961b7e6eca96c96611e9b
Shoichi Kaji <skaji@cpan.org>
2026-05-21 23:25:36 +0900

    Raise minimum Perl to v5.24

---
+++ dist.pl
@@ -0,0 +1,72 @@
+use v5.42;
+
+package Trial {
+    use Moose;
+    with 'Dist::Zilla::Role::FileMunger';
+    sub munge_file ($self, $file) {
+        return if !($ENV{DZIL_RELEASING} && $file->name eq $self->zilla->main_module->name);
+        my @line;
+        for my $line (split /\n/, $file->content, -1) {
+            if ($line =~ /^our \$TRIAL/) {
+                my $trial_line = sprintf 'our $TRIAL = %d;', $self->zilla->is_trial ? 1 : 0;
+                push @line, $trial_line;
+            } else {
+                push @line, $line;
+            }
+        }
+        $file->content(join "\n", @line);
+    }
+}
+
+package NextRelease {
+    use Moose;
+    extends 'Dist::Zilla::Plugin::NextRelease';
+    sub after_release ($self, @) {} # noop
+}
+
+my @prereq = (
+    [ Prereqs => 'ConfigureRequires' ] => [
+        'Module::Build::Tiny' => '0.053',
+    ],
+    [ Prereqs => 'RuntimeRequires' ] => [
+        'App::FatPacker' => '0',
+        'Distribution::Metadata' => '0.05',
+        'Getopt::Long' => '2.39',
+        'Perl::Strip' => '0',
+        'perl' => 'v5.24',
+    ],
+    [ Prereqs => 'DevelopRequires' ] => [
+        'Capture::Tiny' => '0',
+        'File::pushd' => '0',
+    ],
+);
+
+my @plugin = (
+    'ExecDir' => [ dir => 'script' ],
+    'Git::GatherDir' => [ exclude_filename => 'META.json' ],
+    'CopyFilesFromBuild' => [ copy => 'META.json', copy => 'Changes' ],
+    'VersionFromMainModule' => [],
+    'ReversionOnRelease' => [ prompt => 1 ],
+    '=NextRelease' => [ format => '%v  %{yyyy-MM-dd}d%{ (TRIAL RELEASE)}T' ],
+    '=Trial' => [],
+    'Git::Check' => [ allow_dirty => 'Changes', allow_dirty => 'META.json' ],
+    'GithubMeta' => [ issues => 1 ],
+    'ReadmeAnyFromPod' => [ type => 'markdown', filename => 'README.md', location => 'root' ],
+    'MetaProvides::Package' => [ inherit_version => 0, inherit_missing => 0 ],
+    'MetaJSON' => [],
+    'Metadata' => [ x_static_install => 1 ],
+    'Git::Contributors' => [],
+
+    'CheckChangesHasContent' => [],
+    'ConfirmRelease' => [],
+    'FakeRelease' => [],
+    'CopyFilesFromRelease' => [ match => '\.pm$' ],
+    'Git::Commit' => [ commit_msg => '%v%t', allow_dirty => 'Changes', allow_dirty => 'META.json', allow_dirty_match => '\.pm$' ],
+    'Git::Tag' => [ tag_format => '%v%t', tag_message => '%v%t' ],
+    'Git::Push' => [],
+);
+
+my @config = (
+    name => 'App-FatPacker-Simple',
+    [ @prereq, @plugin ],
+);
--- xt/lib/Util.pm
+++ xt/lib/Util.pm
@@ -1,6 +1,7 @@
 package Util;
-use v5.16;
+use v5.24;
 use warnings;
+use experimental qw(lexical_subs signatures);
 
 use Capture::Tiny qw(capture);
 use Cwd 'abs_path';
@@ -15,25 +16,23 @@ our @EXPORT = qw(run spew spew_pm slurp contains pushd tempd);
 my $base = abs_path( File::Spec->catdir( dirname(__FILE__), "..", "..") );
 
 package Result {
-    sub new {
-        my ($class, $out, $err, $exit) = @_;
+    sub new ($class, $out, $err, $exit) {
         bless { exit => $exit, out => $out, err => $err }, $class;
     }
-    sub success {
-        shift->{exit} == 0;
+    sub success ($self) {
+        $self->{exit} == 0;
     }
-    sub out {
-        my $out = shift->{out};
+    sub out ($self) {
+        my $out = $self->{out};
         wantarray ? ( split /\n/, $out ) : $out;
     }
-    sub err {
-        my $err = shift->{err};
+    sub err ($self) {
+        my $err = $self->{err};
         wantarray ? ( split /\n/, $err ) : $err;
     }
 }
 
-sub run {
-    my @argv = @_;
+sub run (@argv) {
     my ($out, $err, $exit) = capture {
         # your responsibility :-)
         # local $ENV{PERL5LIB};
@@ -43,29 +42,25 @@ sub run {
     Result->new($out, $err, $exit);
 }
 
-sub spew {
-    my ($content, $file) = @_;
+sub spew ($content, $file) {
     my $dir = dirname($file);
     mkpath $dir unless -d $dir;
     open my $fh, ">", $file or die "open $file: $!";
     print {$fh} $content;
 }
 
-sub slurp {
-    my $file = shift;
+sub slurp ($file) {
     open my $fh, "<", $file or die "open $file: $!";
     local $/; <$fh>;
 }
 
-sub spew_pm {
-    my ($package, $dir) = @_;
+sub spew_pm ($package, $dir) {
     my $pm = $package;
     $pm =~ s{::}{/}; $pm .= ".pm";
     spew "use $package; 1; # this is comment" => "$dir/$pm";
 }
 
-sub contains {
-    my ($file, $package) = @_;
+sub contains ($file, $package) {
     my $content = slurp $file;
     my $pm = $package;
     $pm =~ s{::}{/}; $pm .= ".pm";
